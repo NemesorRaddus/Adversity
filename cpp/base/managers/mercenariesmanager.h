f45a9dea783d5bf4267d82c2c531644f8e07db64
Raddus <nemesorraddus@gmail.com>
2018-02-21 15:00:50 +0100

    Fix problems that were preventing project from compiling and running
    After switching from MinGW to pure gcc (Win->Linux), some problems appeared.
    
    Add necessary missing Qt #include's in several files.
    Remove duplicate in qrc file.
    Rename Powerplant.png (PowerPlant->Powerplant). And you could think that this madness is over...

--- cpp/base/managers/mercenariesmanager.h
+++ cpp/base/managers/mercenariesmanager.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <QMap>
 #include <QObject>
 
 #include "basemanagerinterface.h"
--- cpp/base/managers/reportsmanager.h
+++ cpp/base/managers/reportsmanager.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QObject>
+#include <QVector>
 
 #include "basemanagerinterface.h"
 
